019792219531dfad417d0ef4fde5cb160e5295b8
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2020-09-05 20:34:26 +0300

    Catch exceptions in InferKit.generate()
    Fixes #8.

--- inferkit.py
+++ inferkit.py
@@ -1,4 +1,5 @@
 import requests
+import simplejson
 
 class InferKit:
 	__URL_PREFIX = 'https://api.inferkit.com/v1/models/'
@@ -16,8 +17,21 @@ class InferKit:
 			},
 			'length': length
 		}
-		r = requests.post(self.url, headers=self._headers, json=data)
-		reply = r.json()
+
+		try:
+			r = requests.post(self.url, headers=self._headers, json=data)
+			r.raise_for_status()
+		except requests.exceptions.RequestException as e:
+			print('\x1b[1;31mRequestException: ' + str(e) + '\x1b[0m')
+			return None
+
+		try:
+			reply = r.json()
+		except simplejson.errors.JSONDecodeError as e:
+			print('\x1b[1;31mJSONDecodeError: ' + str(e) + '\x1b[0m')
+			return None
+
 		if ('data' in reply) and ('text' in reply['data']):
 			return reply['data']['text']
+
 		return None
